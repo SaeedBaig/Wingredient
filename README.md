b01a96b0148654cc03fa62df69c2c48cb74797dd
Toby Harradine <tobyharradine@gmail.com>
2019-09-29 11:17:46 +1000

    Pin all dependencies

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ The *wingredient* directory is our main Python package. This is strucured like a
 4. Set up your editor/IDE if you're using one, so that it uses your virtual environment. In VS Code and PyCharm (and possibly other editors), this means you don't have to activate your virtual environment all the time, as long as you use the integrated terminal.
     - Visual Studio Code: In workspace settings, set the `python.pythonPath` setting to `${workspaceFolder}/.venv/bin/python` on Mac/Linux or `${workspaceFolder}/.venv/Scripts/python.exe` on Windows.
     - PyCharm: Go to *File* -> *Settings* -> *Project* -> *Project Interpreter*, add a Python interpreter, select *Existing environment*, and set the path to the python executable as above, in the VS Code instructions, where `${workspaceFolder}` is the project root directory.
-    - Other editors: Probably a similar process to above
+    - Other editors: Probably a similar process to above, you simply need to tell the editor where to find the Python interpreter executable for this project.
 
 ### Running the Web App
 Provided that you set up as described above, can simply run the web app with the `wingredient` command, in an activated terminal. At the moment it just runs on localhost:5000, but this will probably change at some point.
--- setup.py
+++ setup.py
@@ -4,8 +4,31 @@ setup(
     name="Wingredient",
     version="0.1.0",
     description="Wingredient",
+    # This tells setuptools where to find packages to install. We're simply including the
+    # `wingredient` package and all its sub-packages (if any exist).
     packages=find_packages(include=["wingredient", "wingredient.*"]),
-    install_requires=["flask>=1.1<=1.2", "flask-mako>=0.4<=1.0"],
-    extras_require={"dev": ["black>=19.3b0"]},
+    # These are the core dependencies for the application.
+    # The reason every dependency *and* all of their sub-dependencies are pinned to specific
+    # versions is so we get *deterministic* installations and development environments.
+    # Primary dependencies:
+    # - Flask
+    # - Flask-Mako
+    install_requires=[
+        "Click==7.0",
+        "Flask==1.1.1",
+        "Flask-Mako==0.4",
+        "itsdangerous==1.1.0",
+        "Jinja2==2.10.1",
+        "Mako==1.1.0",
+        "MarkupSafe==1.1.1",
+        "Werkzeug==0.16.0",
+    ],
+    # These are extra/optional requirements which aren't necessary for the application to run, but
+    # are used for developing (a.k.a. development tools)
+    # Primary dependencies:
+    # - black
+    extras_require={"dev": ["appdirs==1.4.3", "attrs==19.1.0", "black==19.3b0", "toml==0.10.0"]},
+    # This tells setuptools to create a script called `wingredient` which simply runs the
+    # `wingredient.__main__` module.
     entry_points={"console_scripts": ["wingredient=wingredient.__main__"]},
 )
