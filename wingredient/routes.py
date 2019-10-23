65139751f9a2e76c80fa5746db045817ecf843f0
Saeed Baig <amateuracademic616@gmail.com>
2019-10-23 23:05:55 +1100

    Can now read what values the user input
    - Added session-variables so we can now read the values (search terms, numbers, checkboxes, etc) the user input on the search page.
    - Clicking the search button will now actually redirect you to the search-results page.
    - Clicking on any recipe on the search-results page will redirect you to the recipe page.

--- wingredient/routes.py
+++ wingredient/routes.py
@@ -1,5 +1,6 @@
-"""Module for the Flask app object."""
-from flask import Flask
+'''Module for the Flask app object.'''
+
+from flask import Flask, request, redirect, url_for, session
 from mako.template import Template
 from os.path import abspath
 
@@ -7,67 +8,89 @@ BASE_DIR = 'wingredient'
 TEMPLATE_DIR = abspath(f'{BASE_DIR}/templates')
 STATIC_DIR = abspath(f'{BASE_DIR}/static')
 
-app = Flask(
-"Wingredient",
-template_folder=TEMPLATE_DIR,
-static_folder=STATIC_DIR)
+app = Flask('Wingredient', template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
 
 #################
 ### HOME PAGE ###
 #################
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def search():
     template = Template(filename=f'{TEMPLATE_DIR}/search.html')
 
+    # If the user clicked search, initialise the session-variables of all the
+    # input fields.
+    if request.method == 'POST':
+        # All of these are str
+        # No user input = empty string
+        for input_field in {'terms', 'num_servings', 'min_rating', 'max_difficulty', 'max_time'}:
+            session[input_field] = request.form[input_field]
+
+        # list of str
+        # No user input = empty list
+        session['ingredients'] = request.form.getlist('ingredients')
+
+        # All of these are bool
+        # Unselected = False
+        for checkbox in {'pantry_only', 'vegan', 'vegetarian', 'dairy_free', 'gluten_free'}:
+            session[checkbox] = (checkbox in request.form)
+
+        return redirect(url_for('results'))
+
     # This list of ingredients is hard-coded;
     # it should probably be pulled out of the database
-    return template.render(
-    ingredients=['Milk', 'Flour', 'Sugar', 'Vanilla', 'Eggs']
-    )
+    return template.render(ingredients=['Milk', 'Flour', 'Sugar', 'Vanilla', 'Eggs'])
 
 
 ###########################
 ### SEARCH RESULTS PAGE ###
 ###########################
-@app.route("/results")
-def search_results():
+@app.route('/results')
+def results():
     template = Template(filename=f'{TEMPLATE_DIR}/search-results.html')
+
+    # All the session-variables initialised in search() are available here.
+    # E.g.
+    print(session['ingredients'])
+    print(session['num_servings'])
+    print(session['vegan'])
+    # Process the variables in whatever way you need to fetch the correct
+    # search results
 
     # All these paramaters are hard-coded;
     # they should probably be pulled out of the database
     return template.render(
-    titles=['Bowl of Cereal', 'Omellete', 'Stir Fry'],
-    image_paths=['static/bowl of cereal.jpg', 'static/omellete.jpg',
-    'static/stir fry.jpg'],
-    image_alts=['bowl of cereal', 'omellete', 'stir fry'],
-    ratings=[74, 86, 91],
-    cooking_times_in_minutes=[2, 18, 34],
-    links_to_recipe = ['#', '#', '#']
+        titles=['Bowl of Cereal', 'Omellete', 'Stir Fry'],
+        image_paths=['static/bowl of cereal.jpg', 'static/omellete.jpg', 'static/stir fry.jpg'],
+        image_alts=['bowl of cereal', 'omellete', 'stir fry'],
+        ratings=[74, 86, 91],
+        cooking_times_in_minutes=[2, 18, 34],
+        links_to_recipe=['/recipe', '/recipe', '/recipe'],
     )
 
 
 ###########################
 ### SEARCH RECIPE PAGE ####
 ###########################
-@app.route("/recipe")
+@app.route('/recipe')
 def recipe():
     template = Template(filename=f'{TEMPLATE_DIR}/recipe.html')
 
     # All these paramaters are hard-coded;
     # they should probably be pulled out of the database
     return template.render(
-    title='Bowl of Cereal',
-    image_path='static/bowl of cereal.jpg',
-    image_alt='bowl of cereal',
-    cooking_time_in_minutes=2.5,
-    difficulty='Medium',    # can be 'Easy', 'Medium', or 'Hard'
-    ingredients=['Milk', 'Cereal'],
-    equipment=['Bowl', 'Cup', 'Microwave'],
-    method = [
-    'Fill bowl with cereal.',
-    'Fill cup with milk.',
-    'Warm up cup in microwave for 1 minute.',
-    'Pour cup of milk into bowl of cereal.'],
-    num_likes=128
+        title='Bowl of Cereal',
+        image_path='static/bowl of cereal.jpg',
+        image_alt='bowl of cereal',
+        cooking_time_in_minutes=2.5,
+        difficulty='Medium',  # can be 'Easy', 'Medium', or 'Hard'
+        ingredients=['Milk', 'Cereal'],
+        equipment=['Bowl', 'Cup', 'Microwave'],
+        method=[
+            'Fill bowl with cereal.',
+            'Fill cup with milk.',
+            'Warm up cup in microwave for 1 minute.',
+            'Pour cup of milk into bowl of cereal.',
+        ],
+        num_likes=128,
     )
