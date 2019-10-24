988f08f18445e31a23020bd42a632bae5e166e31
Andrew Ross <ar85ar@gmail.com>
2019-10-24 15:13:19 +1100

    Create stub load_user()

--- wingredient/user.py
+++ wingredient/user.py
@@ -1,3 +1,5 @@
+from .routes import login_manager
+
 class User:
     def __init__(self):
         self.is_authenticated = False
@@ -7,3 +9,9 @@ class User:
     def get_id(self):
         # placeholder username
         return 'guest'
+        
+
+@login_manaer.user_loader
+def load_user(user_id):
+    # TODO Create user object from unicode user_id
+    return None
