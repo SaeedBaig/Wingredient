0c3afc269dca886e44c4b78d82a2a11ae99a29e1
keVinDiesel <z5165218@cse.unsw.edu.au>
2019-10-29 15:45:03 +1100

    add updates file

---
+++ wingredient/db/wingredient-updates.sql
@@ -0,0 +1,15 @@
+-- modifications made to the database to assist with SQL queries
+CREATE OR REPLACE VIEW compulsory_recipetoingredient as
+select *
+from recipetoingredient
+where optional = 'false'
+;
+
+
+CREATE OR REPLACE VIEW ingredient_counts as
+select recipe, count(recipe)
+from compulsory_recipetoingredient
+group by recipe
+;
+
+
