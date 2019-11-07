d7b8decba782a21de9c8d34644cf1de2d29166d5
keVinDiesel <z5165218@cse.unsw.edu.au>
2019-11-07 15:48:52 +1100

    moved view functions

--- wingredient/dbdata/wingredient-schema.sql
+++ wingredient/dbdata/wingredient-schema.sql
@@ -115,3 +115,19 @@ CREATE TABLE Dislikes (
     recipe      integer references Recipe(id),
     primary key (account, recipe)
 );
+
+
+
+-- modifications made to the database to assist with SQL queries
+CREATE OR REPLACE VIEW compulsory_recipetoingredient as
+select *
+from recipetoingredient
+where optional = 'false'
+;
+
+
+CREATE OR REPLACE VIEW ingredient_counts as
+select recipe, count(recipe)
+from compulsory_recipetoingredient
+group by recipe
+;
