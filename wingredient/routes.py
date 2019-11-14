071f336d2195a85305156430a35490ecc5b64959
Toby Harradine <tobyharradine@gmail.com>
2019-11-14 11:51:07 +1100

    Use URL parameters rather than session cookies

--- wingredient/routes.py
+++ wingredient/routes.py
@@ -5,6 +5,9 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 from mako.runtime import Context
 from os.path import abspath
+
+from werkzeug.datastructures import ImmutableMultiDict
+
 from . import db
 from collections import Counter
 from flask_login import LoginManager, current_user, login_user, logout_user
@@ -38,24 +41,32 @@ def search():
     if request.method == "POST":
         # All of these are str
         # No user input = empty string
-        for input_field in {"terms", "num_servings", "min_rating", "max_difficulty", "max_time"}:
-            session[input_field] = request.form[input_field]
+        url_args = {}
+        for input_field in ("terms", "num_servings", "min_rating", "max_difficulty", "max_time"):
+            if request.form[input_field]:
+                url_args[input_field] = request.form[input_field]
 
         # list of str
         # No user input = empty list
         ingredients = request.form['ingredients'].split(',')
-        if ingredients == ['']: # no user input
-            ingredients = []
-        session["ingredients"] = ingredients
+        if ingredients != ['']: # no user input
+            url_args["ingredients"] = ingredients
 
-        session["pantry_only"] = request.form.get("pantry_only", False)
+        if request.form.get("pantry_only"):
+            url_args["pantry_only"] = True
+
         dietary_tags = 0
         for i, checkbox in enumerate(("vegetarian", "vegan", "gluten_free", "dairy_free")):
             if request.form.get(checkbox):
                 dietary_tags |= 1 << i
-        session["dietary_tags"] = dietary_tags
 
-        return redirect(url_for("results"))
+        if dietary_tags:
+            url_args["dietary_tags"] = dietary_tags
+
+        return redirect(url_for(
+            "results",
+            **url_args,
+        ))
 
     # This list of ingredients is hard-coded;
     # it should probably be pulled out of the database
@@ -87,13 +98,13 @@ def results():
 
     # All the session-variables initialised in search() are available here.
     # E.g.
-    print(session["ingredients"])
-    print(session["num_servings"])
+    print(request.args.getlist("ingredients"))
+    print(request.args.get("num_servings"))
     # Process the variables in whatever way you need to fetch the correct
     # search results
 
-    results = get_search(session["ingredients"], session["dietary_tags"], session["max_time"])
-    if results == -1:
+    _results = get_search(request.args)
+    if _results == -1:
         return template.render(
             titles=""
         )
@@ -102,12 +113,12 @@ def results():
     # All these paramaters are hard-coded;
     # they should probably be pulled out of the database
     return template.render(
-        titles=[r[1] for r in results],  #name from recipe
-        image_paths=[r[4] for r in results],    # imageRef from recipe
-        image_alts=[r[3] for r in results],  # set to description from recipe
-        ratings=[80 for r in results],
-        cooking_times_in_minutes=[r[2] for r in results],                   #time from recipe
-        recipe_ids=[r[0] for r in results],
+        titles=[r[1] for r in _results],  # name from recipe
+        image_paths=[r[4] for r in _results],    # imageRef from recipe
+        image_alts=[r[3] for r in _results],  # set to description from recipe
+        ratings=[80 for r in _results],
+        cooking_times_in_minutes=[r[2] for r in _results],                   #time from recipe
+        recipe_ids=[r[0] for r in _results],
         default='alphabetical'
     )
 
@@ -115,7 +126,7 @@ def results():
 def results_post():
     template = LOOKUP.get_template("search-results.html")
 
-    results = get_search(session["ingredients"])
+    results = get_search(request.args)
     if results == -1:
         return template.render(
             titles=""
@@ -139,25 +150,33 @@ def results_post():
     )
 
 
-def get_search(ingredients, dietary_tags=0, max_time=0):
+def get_search(request_args: ImmutableMultiDict):
     with db.getconn() as conn:
         with conn.cursor() as cursor:
             whereclauses = []
-            args = []
+            query_args = {}
+
+            ingredients = request_args.getlist('ingredients')
             if ingredients:
-                whereclauses.append("i.name IN %s")
-                args.append(tuple(ingredients))
+                whereclauses.append("i.name IN %(ingredients)s")
+                query_args["ingredients"] = tuple(ingredients)
                 missing_ingredient_count_expr = (
                     "ic.compulsory_ingredient_count - count(rtoi.recipe)"
                 )
             else:
                 missing_ingredient_count_expr = "ic.compulsory_ingredient_count"
+
+            dietary_tags = request_args.get('dietary_tags', default=0, type=int)
             if dietary_tags:
-                whereclauses.append("r.dietary_tags & %s::bit(4) <> b'0000'")
-                args.append(dietary_tags)
+                whereclauses.append(
+                    "r.dietary_tags & %(dietary_tags)s::bit(4) = %(dietary_tags)s::bit(4)"
+                )
+                query_args["dietary_tags"] = dietary_tags
+
+            max_time = request_args.get('max_time', default=0, type=int)
             if max_time:
-                whereclauses.append("r.time <= %s")
-                args.append(max_time)
+                whereclauses.append("r.time <= %(max_time)s")
+                query_args["max_time"] = max_time
 
             if whereclauses:
                 whereclause = "WHERE " + " AND ".join(whereclauses)
@@ -186,7 +205,7 @@ def get_search(ingredients, dietary_tags=0, max_time=0):
                   missing_ingredient_count,
                   r.name
             """
-            cursor.execute(query, args)
+            cursor.execute(query, query_args)
             return cursor.fetchall()
 
 ###########################
