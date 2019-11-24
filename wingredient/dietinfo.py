9f7164658e958a45ed0545e20cf03a56a5123429
Andrew Ross <ar85ar@gmail.com>
2019-11-24 14:44:39 +1100

    Fucntions for cunverting diet bitsrings

--- wingredient/dietinfo.py
+++ wingredient/dietinfo.py
@@ -5,3 +5,23 @@ allowed_diets = [
     "Dairy-Free",
     "Gluten-Free"
 ]
+
+allowed_diets_short = {
+    "Vegan" : "VGN",
+    "Vegetarian" : "VGT",
+    "Dairy-Free" : "DF",
+    "Gluten-Free" : "GF"
+}
+
+
+def diet_bits_to_names(dietary_tags):
+    bitstring = 1
+    active_diets = []
+    for diet in allowed_diets:
+        if bitstring & dietary_tags:
+            active_diets.append(diet)
+        bitstring << 1
+    return active_diets
+
+def diet_bits_to_short_names(dietary_tags):
+    return list(map(lambda x: allowed_diets_short[x], diet_bits_to_names(dietary_tags))
